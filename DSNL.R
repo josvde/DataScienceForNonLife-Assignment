2310129a951ddb342f9e598c54859fe063391d54
josvde <josvde99@gmail.com>
2023-05-13 21:14:00 +0200

    dynamic working directory added
    working directory will now always the folder where the current script is stored at

--- DSNL.R
+++ DSNL.R
@@ -18,7 +18,9 @@ library(dplyr)
 
 # 0. Import data
 
-setwd("C:/Users/rdessein/oneDrive - Deloitte (O365D)/Documents/GitHub/DataScienceForNonLife-Assignment")
+dir <- dirname(rstudioapi::getActiveDocumentContext()$path)
+setwd(dir)
+rm(dir)
 
 ### 0.1 Data of "Assinment.csv" and "inspost.xls" merged using VLOOKUP
 
