f49ced53ff497c4534204e4552c2e0b1cb322550
josvde <josvde99@gmail.com>
2023-05-19 13:05:47 +0200

    ETR and factor conversion ready

--- GLM.R
+++ GLM.R
@@ -1,4 +1,4 @@
-## Intro  ##
+## Intro  ####
 
 # we will first investigate for each covariate what levels yield the highest exposure to risk...
 # exposure to risk =total number of claims*average claim amount/total number of years
@@ -17,7 +17,7 @@ library(sf) # for spatial data manipulation
 library(tmap) # for creating maps
 library(gridExtra)
 
-## 0. Loading and manipulating data ##
+## 0. Loading and manipulating data ####
 dir <- dirname(rstudioapi::getActiveDocumentContext()$path)
 setwd(dir)
 rm(dir)
@@ -51,9 +51,9 @@ a[a==9]= "Namur"
 Data$INS<-a
 rm(a)
 
-## 1. Poisson Regression to build frequency table for technical tariff plan##
+## 1. Poisson Regression to build frequency table for technical tariff plan####
 
-# 1.1. Exposure to risk
+# 1.1. Exposure to risk ####
 # exposure to risk = average claim cost per policy / average policy duration
 
 # Exposure to risk by age policy holder
@@ -96,24 +96,74 @@ ETR_split <- Data %>%
 
 # conclusion: set monthly as reference level
 
-# Exposure to risk by fuelc
+# Exposure to risk by usec
 
-ETR_fuelc <- Data %>%
-  group_by(fuelc) %>%
+ETR_usec <- Data %>%
+  group_by(usec) %>%
   summarise(ETR = mean(chargtot)/mean(duree))
 
-# conclusion: set gasoil as reference level
+# conclusion: set private as reference level
 
-# 1.2. construction of data frame
+# Exposure to risk by fleetc
 
-# straightforward conversions to factor variables
-  
-# we need a data frame where we have the response (number of claims), exposure (duration of contract),
-# and all covariates in factor from.
+ETR_fleetc <- Data %>%
+  group_by(fleetc) %>%
+  summarise(ETR = mean(chargtot)/mean(duree))
 
-  # straightforward conversions to factor variables
+# conclusion: set "No" as reference level
+
+# Exposure to risk by sportc
+
+ETR_sportc <- Data %>%
+  group_by(sportc) %>%
+  summarise(ETR = mean(chargtot)/mean(duree))
+
+# conclusion: set "Yes" as reference level
+
+# Exposure to risk by coverp
+
+ETR_coverp <- Data %>%
+  group_by(coverp) %>%
+  summarise(ETR = mean(chargtot)/mean(duree))
+
+# conclusion: set MTPL as reference level
+
+# Exposure to risk by powerc
+
+ETR_powerc <- Data %>%
+  group_by(powerc) %>%
+  summarise(ETR = mean(chargtot)/mean(duree))
+
+# conclusion: set >110 as reference level
+
+# Exposure to risk by INS (province)
+
+ETR_INS <- Data %>%
+  group_by(INS) %>%
+  summarise(ETR = mean(chargtot)/mean(duree))
+
+# conclusion: set West Flanders as reference level
+
+# 1.2. construction of data frame ####
+
+# conversions of all covariates to factor variables, with the reference level as first level.
+# summary of reference level: age group 17-36, car age 0-1, female, gasoil, monthly payments, private car,...
+# No to fleet car, Yes to sportcar, policy type MTPL, power of car >110 and finally West Flanders.
+
+Data$AGEPH <- factor(Data$AGEPH, levels=c("17-36","37-56","57-76",">76"))
+Data$agecar <- factor(Data$agecar, levels=c("0-1","2-5","6-10",">10"))
+Data$sexp <- factor(Data$sexp, levels=c("female","male"))
+Data$fuelc <- factor(Data$fuelc, levels=c("Gasoil","Petrol"))
+Data$split <- factor(Data$split, levels=c("Monthly","Trice","Twice","Once"))
+Data$usec <- factor(Data$usec, levels=c("Private","Professional"))
+Data$fleetc <- factor(Data$fleetc, levels=c("No","Yes"))
+Data$sportc <- factor(Data$sportc, levels=c("Yes","No"))
+Data$coverp <- factor(Data$coverp, levels=c("MTPL","MTPL+","MTPL+++"))
+Data$powerc <- factor(Data$powerc, levels=c(">110","<66","66-110"))
+Data$INS <- factor(Data$INS,levels=c("West Flanders","Antwerp","Brabant & BXL","East Flanders","Hainaut","Liege","Limburg","Luxembourg","Namur"))
+
 # 1.3. Resulting GLM & tariff table
-# GLM1 <- glm(response ~ covariate1 + ... + covariateN + offset(log(expo)), data= DF, family=poisson(link="log"))
+GLMFull <- glm(nbrtotc ~ AGEPH +  + covariateN + offset(log(expo)), data= DF, family=poisson(link="log"))
 
 # to build the tariff table we will use: GLM1 %>% broom::augment(type.predict ="response")
 
